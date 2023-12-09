1487fc38a8eb351b119ed801f52bfab7decf4476
MisterNegativ <120665517+MisterNegativ@users.noreply.github.com>
2023-12-09 22:56:11 +0600

    Настройка заголовка

---
+++ src/components/elements/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="rmdb-header">
+            <div className="rmdb-header-content">
+                <img src="./images/reactMovie_logo.png"
+                    className="rmdb-logo"
+                    alt="rmdb-logo"/>
+                <img src="../images/tmdb_logo.png"
+                    className="rmdb-tmdb-logo"
+                    alt="rmdb-tmdb-logo"/>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
