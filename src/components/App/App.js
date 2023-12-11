0674be8daf24a542459741650f89d90e79b23bf7
MisterNegativ <120665517+MisterNegativ@users.noreply.github.com>
2023-12-11 21:50:51 +0600

    Добавление новых компонентов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../elements/Header/Header";
 import Home from "../Home/Home";
+import Movie from "../Movie/Movie";
+import NotFound from "../elements/NotFound/NotFound";
 
 class App extends React.Component{
     render() {
         return(
-            <div>
+        <BrowserRouter>
+            <React.Fragment>
                 <Header />
-                <Home />
-            </div>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/:movieId" component={Movie} exact />
+                    <Route component={NotFound} />
+                </Switch>
+            </React.Fragment>
+        </BrowserRouter>
         );
     }
 }
--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
     return (
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img src="./images/reactMovie_logo.png"
+            <Link to="/">
+                <img
+                    src="./images/reactMovie_logo.png"
                     className="rmdb-logo"
-                    alt="rmdb-logo"/>
+                    alt="rmdb-logo"
+                />
+            </Link> 
                 <img src="../images/tmdb_logo.png"
                     className="rmdb-tmdb-logo"
                     alt="rmdb-tmdb-logo"/>
